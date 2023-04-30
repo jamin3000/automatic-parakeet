3c4194521861c402fb93fabd503f7d1394dd7237
jamin3000 <98733081+jamin3000@users.noreply.github.com>
2023-04-30 21:33:00 +1000

    Update route.ts
    needs to ts'd

--- app/api/story/route.ts
+++ app/api/story/route.ts
@@ -17,8 +17,26 @@ export async function POST(req: Request): Promise<Response> {
   if (!prompt) {
     return new Response('No prompt in the request', { status: 400 });
   }
+  
+  //* python
+  
+  industry = input("What industry are you writing to?")
+  person = input("What is their role?")
+  product = input("What is your product?")
+  
+  def token_limiter(content):
+    //* 1 token is appropximately 4 characters, if we overshoot then damn
+    tokens_start = 4000
+    tokens_content = len(content)*4
+    tokens_available = token_start - tokens_content
+    return tokens_available
+  
+  
+  
+  content = f"You are a salesperson selling your {product} and are writing a sales email to a client: {person} who works in {industry}. Write a 100 word email pitching the product. Start the email with a joke about the client's industry."
+  
 
-  const content = `Create a short ${genre} story, no more than 120 words, in the voice of philosoper Nietzsche based on the following topic: ${prompt}.`;
+  //const content = `Create a short ${genre} story, no more than 100 words, in the voice of philosoper Nietzsche based on the following topic: ${prompt}.`;
 
   const payload: OpenAIStreamPayload = {
     model: 'gpt-3.5-turbo',
@@ -28,11 +46,12 @@ export async function POST(req: Request): Promise<Response> {
         content,
       },
     ],
-    temperature: 0.8,
+    //changed temperature
+    temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 1000,
+    max_tokens: tokens_available,
     stream: true,
     n: 1,
   };
